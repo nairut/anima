8e03552f9a9e78228a63d974d521d35500b44e1d
nairut <turian@hotmail.com>
2023-07-13 10:55:19 -0300

    Update index.js

--- src/screens/Element/index.js
+++ src/screens/Element/index.js
@@ -1,1 +1,17 @@
 export { Element } from "./Element";
+import React from "react";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-K3GKPJ5'
+}
+
+TagManager.initialize(tagManagerArgs)
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    document.getElementById('root')
+)
