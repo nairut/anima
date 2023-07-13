454b327ccd45bfda31b840ae92f7dbb59c99af5e
nairut <turian@hotmail.com>
2023-07-13 16:36:14 -0300

    sadfsdfsfd

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Group } from "../../components/Group";
 import { Lady } from "../../components/Lady";
-import { MagmaTranslation } from "../../components/MagmaTranslation";
 import "./style.css";
 
 export const Desktop = () => {
   return (
     <div className="desktop">
-      <div className="div">
-        <div className="group-2">
-          <h1 className="text-wrapper">Professional Translation Services</h1>
-          <p className="element-languages-that">
-            120 languages that fits in your budget
-            <br />
-            Human Translation done with the highest quality in the market
-            <br /> Linguists with Master&#39;s Degrees
-          </p>
-          <Group className="group-instance" talkToAnExpertClassName="group-2-instance" />
+      <div className="frame-wrapper">
+        <div className="frame">
+          <div className="frame-2">
+            <h1 className="h-1">Professional Translation Services</h1>
+            <p className="element-languages-that-2">
+              120 languages that fits in your budget
+              <br />
+              Human Translation done with the highest quality in the market
+              <br /> Linguists with Master&#39;s Degrees
+            </p>
+            <Group className="design-component-instance-node" talkToAnExpertClassName="group-3" />
+          </div>
+          <div className="lady-wrapper">
+            <Lady className="lady-2" />
+          </div>
         </div>
-        <Lady className="lady-instance" />
-        <MagmaTranslation className="magma-translation-png" />
-        <p className="p">Trusted by Elite Businesses Worldwide</p>
-        <img className="img" alt="Group" src="/img/group-6.png" />
       </div>
     </div>
   );
---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Group } from "../../components/Group";
+import { Lady } from "../../components/Lady";
+import { MagmaTranslation } from "../../components/MagmaTranslation";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="group-2">
+          <h1 className="text-wrapper">Professional Translation Services</h1>
+          <p className="element-languages-that">
+            120 languages that fits in your budget
+            <br />
+            Human Translation done with the highest quality in the market
+            <br /> Linguists with Master&#39;s Degrees
+          </p>
+          <Group className="group-instance" talkToAnExpertClassName="group-2-instance" />
+        </div>
+        <Lady className="lady-instance" />
+        <MagmaTranslation className="magma-translation-png" />
+        <p className="p">Trusted by Elite Businesses Worldwide</p>
+        <img className="img" alt="Group" src="/img/group-6-4.png" />
+      </div>
+    </div>
+  );
+};
