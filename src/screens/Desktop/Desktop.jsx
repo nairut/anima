be9522b45ec1aab4830dec0d93e28ebc5c186fcc
nairut <turian@hotmail.com>
2023-07-13 14:24:35 -0300

    newnewewe

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -8,19 +8,20 @@ export const Desktop = () => {
   return (
     <div className="desktop">
       <div className="div">
-        <h1 className="text-wrapper">Professional Translation Services</h1>
-        <p className="element-languages-that">
-          120 languages that fits in your budget
-          <br />
-          11,000+ translators
-          <br />
-          Easy, Technological, Professional
-        </p>
-        <Group className="group-2" talkToAnExpertClassName="group-instance" />
+        <div className="group-2">
+          <h1 className="text-wrapper">Professional Translation Services</h1>
+          <p className="element-languages-that">
+            120 languages that fits in your budget
+            <br />
+            Human Translation done with the highest quality in the market
+            <br /> Linguists with Master&#39;s Degrees
+          </p>
+          <Group className="group-instance" talkToAnExpertClassName="group-2-instance" />
+        </div>
         <Lady className="lady-instance" />
         <MagmaTranslation className="magma-translation-png" />
-        <img className="img" alt="Group" src="/img/group-4.png" />
         <p className="p">Trusted by Elite Businesses Worldwide</p>
+        <img className="img" alt="Group" src="/img/group-6.png" />
       </div>
     </div>
   );
