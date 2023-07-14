35b778cb2b5e921aeac7f10e8b635aa87534b3a7
nairut <turian@hotmail.com>
2023-07-14 14:22:05 -0300

    sdfsdf

--- src/components/Group/Group.jsx
+++ src/components/Group/Group.jsx
@@ -1,45 +1,54 @@
-import React, { useState } from "react";
-import { Group } from "../../components/Group";
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import PropTypes from "prop-types";
+import React from "react";
+import { useReducer } from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
-export const Desktop = () => {
-  const [name, setName] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission logic here
-    console.log("Form submitted with name:", name);
-  };
-
-  const handleChange = (e) => {
-    setName(e.target.value);
-  };
+export const Group = ({ property1, className, to }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    property1: property1 || "default",
+  });
 
   return (
-    <div className="desktop">
-      <div className="div">
-        <form
-          className="overlap-group"
-          onSubmit={handleSubmit}
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          name="Turian"
-        >
-          <div className="rectangle" />
-          <input
-            className="name"
-            type="text"
-            value={name}
-            onChange={handleChange}
-            placeholder="Enter your name"
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <Group className="group-2" property1="default" to="/thank-you">
-          <button>TALK TO AN EXPERT</button>
-        </Group>
-      </div>
-    </div>
+    <Link
+      className={`group ${state.property1} ${className}`}
+      to={to}
+      onMouseEnter={() => {
+        dispatch("mouse_enter");
+      }}
+      onMouseLeave={() => {
+        dispatch("mouse_leave");
+      }}
+    >
+      <div className="talk-to-an-expert">TALK TO AN EXPERT</div>
+    </Link>
   );
+};
+
+function reducer(state, action) {
+  switch (action) {
+    case "mouse_enter":
+      return {
+        ...state,
+        property1: "variant-2",
+      };
+
+    case "mouse_leave":
+      return {
+        ...state,
+        property1: "default",
+      };
+  }
+
+  return state;
+}
+
+Group.propTypes = {
+  property1: PropTypes.oneOf(["variant-2", "default"]),
+  to: PropTypes.string,
 };
