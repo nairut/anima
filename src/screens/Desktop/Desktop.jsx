1dea36b667f2fe47cc90e397eb985c493b0b78ea
nairut <turian@hotmail.com>
2023-07-14 14:03:09 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -10,13 +10,51 @@ export const Desktop = () => {
           <div className="rectangle" />
           <input className="name" />
         </div>
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-  {/* You still need to add the hidden input with the form name to your JSX form */}
-  <input type="hidden" name="form-name" value="contact" />
-  </form>
         <Group className="group-2" property1="default" to="/thank-you" />
       </div>
     </div>
   );
 };
 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HTMLPage = () => {
+  const yourHTMLContent = `<!DOCTYPE HTML>
+    <!--
+    Paradigm Shift by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    -->
+    <html>
+    <head>
+        <title>Paradigm Shift by HTML5 UP</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+    </head>
+    <body class="is-preload">
+        <!-- Wrapper -->
+        <div id="wrapper">
+            <!-- Your HTML content here -->
+        </div>
+
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.scrolly.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+    </html>`;
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: yourHTMLContent }} />
+  );
+};
+
+ReactDOM.render(<HTMLPage />, document.getElementById('root'));
