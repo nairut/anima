593bf104842de9004b41c7706fdea19ddfcda3c6
nairut <turian@hotmail.com>
2023-07-14 13:44:27 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -1,57 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { Group } from "../../components/Group";
 import "./style.css";
 
 export const Desktop = () => {
-  const [name, setName] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Perform the form submission logic here
-      const response = await fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "my-form", name }),
-      });
-      if (response.ok) {
-        // Handle successful form submission
-        console.log("Form submitted successfully");
-        // Reset the input field after submission
-        setName("");
-      } else {
-        // Handle form submission error
-        console.error("Form submission failed");
-      }
-    } catch (error) {
-      // Handle form submission error
-      console.error("Form submission failed:", error);
-    }
-  };
-
-  const encode = (data) => {
-    return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-  };
-
   return (
     <div className="desktop">
       <div className="div">
         <div className="overlap-group">
           <div className="rectangle" />
-          <form onSubmit={handleSubmit} netlify netlify-honeypot="bot-field">
-            <input type="hidden" name="form-name" value="my-form" />
-            <input
-              className="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-          </form>
+          <input className="name" />
         </div>
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+  {/* You still need to add the hidden input with the form name to your JSX form */}
+  <input type="hidden" name="form-name" value="contact" />
+  </form>
         <Group className="group-2" property1="default" to="/thank-you" />
       </div>
     </div>
   );
 };
+
