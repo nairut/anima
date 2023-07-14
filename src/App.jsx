d13b771e76d02f833adc46afe0c0c8b3e30e8fae
nairut <turian@hotmail.com>
2023-07-14 10:57:10 -0300

    sdfdfsdf

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Desktop } from "./screens/Desktop";
+import { ThankYou } from "./screens/ThankYou";
+
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <Desktop />,
+  },
+  {
+    path: "/desktop-1",
+    element: <Desktop />,
+  },
+  {
+    path: "/thank-you",
+    element: <ThankYou />,
+  },
+]);
+
+export const App = () => {
+  return <RouterProvider router={router} />;
+};
