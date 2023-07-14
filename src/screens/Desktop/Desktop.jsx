ae2578b0d2d3af07e06b2314340998f0b0b0791b
nairut <turian@hotmail.com>
2023-07-14 14:07:43 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -1,60 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Group } from "../../components/Group";
 import "./style.css";
 
 export const Desktop = () => {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted with name:", name);
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div className="desktop">
       <div className="div">
-        <div className="overlap-group">
+        <form
+          className="overlap-group"
+          onSubmit={handleSubmit}
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
           <div className="rectangle" />
-          <input className="name" />
-        </div>
+          <input
+            className="name"
+            type="text"
+            value={name}
+            onChange={handleChange}
+            placeholder="Enter your name"
+          />
+          <button type="submit">Submit</button>
+        </form>
         <Group className="group-2" property1="default" to="/thank-you" />
       </div>
     </div>
   );
 };
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const HTMLPage = () => {
-  const yourHTMLContent = `<!DOCTYPE HTML>
-    <!--
-    Paradigm Shift by HTML5 UP
-    html5up.net | @ajlkn
-    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-    -->
-    <html>
-    <head>
-        <title>Paradigm Shift by HTML5 UP</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <link rel="stylesheet" href="assets/css/main.css" />
-    </head>
-    <body class="is-preload">
-        <!-- Wrapper -->
-        <div id="wrapper">
-            <!-- Your HTML content here -->
-        </div>
-
-        <!-- Scripts -->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/jquery.scrolly.min.js"></script>
-        <script src="assets/js/browser.min.js"></script>
-        <script src="assets/js/breakpoints.min.js"></script>
-        <script src="assets/js/util.js"></script>
-        <script src="assets/js/main.js"></script>
-    </body>
-    </html>`;
-
-  return (
-    <div dangerouslySetInnerHTML={{ __html: yourHTMLContent }} />
-  );
-};
-
-ReactDOM.render(<HTMLPage />, document.getElementById('root'));
