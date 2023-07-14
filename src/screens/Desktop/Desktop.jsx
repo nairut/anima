dbaceaf11a5f1b29ec49ce5b1d3ec377c964ef84
nairut <turian@hotmail.com>
2023-07-14 14:22:54 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Group } from "../../components/Group";
 import "./style.css";
 
 export const Desktop = () => {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted with name:", name);
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div className="desktop">
       <div className="div">
-        <div className="overlap-group">
+        <form
+          className="overlap-group"
+          onSubmit={handleSubmit}
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
           <div className="rectangle" />
-          <input className="name" />
-        </div>
-        <Group className="group-2" property1="default" to="/thank-you" />
+          <input
+            className="name"
+            type="text"
+            value={name}
+            onChange={handleChange}
+            placeholder="Enter your name"
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <Group className="group-2" property1="default" to="/thank-you">
+          <button>TALK TO AN EXPERT</button>
+        </Group>
       </div>
     </div>
   );
