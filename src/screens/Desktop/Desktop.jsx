2ae9db044a61734d6166d607390d9f5faca340ab
nairut <turian@hotmail.com>
2023-07-14 15:45:09 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -15,10 +15,3 @@ export const Desktop = () => {
     </div>
   );
 };
-
-
-<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-  {/* You still need to add the hidden input with the form name to your JSX form */}
-  <input type="hidden" name="form-name" value="contact" />
-  ...
-</form>
