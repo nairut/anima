4e30e0790b15e58cb50417470f5c7281fdc2abe5
nairut <turian@hotmail.com>
2023-07-14 11:06:04 -0300

    sdfsdf

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Group } from "../../components/Group";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <input className="name" />
+        </div>
+        <Group className="group-2" talkToAnExpertClassName="group-instance" />
+      </div>
+    </div>
+  );
+};
