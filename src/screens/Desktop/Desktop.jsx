61f989633b07eccc76eb0c66b6073d31f2e2553a
nairut <turian@hotmail.com>
2023-07-17 19:02:08 -0300

    sdfsdf

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -6,16 +6,34 @@ export const Desktop = () => {
   return (
     <div className="desktop">
       <div className="div">
+
+
+
+      <form name="contact" method="post" action="/contact">
+      <input type="hidden" name="form-name" value="contact" />
         <div className="overlap">
           <div className="rectangle" />
-          <input className="name" />
+          <input type="text" name="name" className="name" placeholder="Enter your name" />
         </div>
+
+
+        <button  type="submit" >
         <div className="overlap-group">
-          <div className="rectangle-2" />
           <div className="talk-to-an-expert">TALK TO AN EXPERT</div>
         </div>
+        </button>
+        </form>
+
+
+
+
         <Lady className="lady-instance" />
       </div>
     </div>
   );
 };
+
+
+
+
+
