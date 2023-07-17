f38618b2452022bc06895d6856571fb971d15e4d
nairut <turian@hotmail.com>
2023-07-17 16:40:25 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -16,10 +16,10 @@ export const Desktop = () => {
           <form name="contact" method="post" action="/contact">
           <input type="hidden" name="form-name" value="contact" />
             <input type="text" className="name" placeholder="Enter your name" />
-            <button type="submit">asdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçkl</button>
+            <button className="TalkToAnExpert"  type="submit">lsçklasdfladsçklasdfladsçklasdfladsçkl</button>
           </form>
         </div>
-        <Group className="group-2" property1="default" to="/thank-you" />
+
       </div>
     </div>
   );
