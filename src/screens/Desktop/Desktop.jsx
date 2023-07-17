c67972652d771ebf13b8afd0e2e84c42bf72d000
nairut <turian@hotmail.com>
2023-07-17 16:15:28 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -18,9 +18,18 @@ export const Desktop = () => {
 </button>
           </form>
 
+
+          <div className="Group2 w-[335px] h-[55px] relative">
+  <div className="Rectangle1 w-[335px] h-[55px] left-0 top-0 absolute bg-blue-800" />
+  <div className="TalkToAnExpert w-[209px] h-[9px] left-[63px] top-[23px] absolute text-center text-white text-xl font-light uppercase leading-[13px] tracking-wide">Talk to an Expert</div>
+</div>
+
         </div>
         
       </div>
     </div>
   );
 };
+
+
+
