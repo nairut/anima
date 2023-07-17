9679c5dfcba712f537bfb022811669bdb2d9a056
nairut <turian@hotmail.com>
2023-07-17 13:50:33 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -3,15 +3,24 @@ import { Group } from "../../components/Group";
 import "./style.css";
 
 export const Desktop = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+  };
+
   return (
     <div className="desktop">
       <div className="div">
         <div className="overlap-group">
           <div className="rectangle" />
-          <input className="name" />
+          <form onSubmit={handleSubmit}>
+            <input type="text" className="name" placeholder="Enter your name" />
+            <button type="submit">Submit</button>
+          </form>
         </div>
         <Group className="group-2" property1="default" to="/thank-you" />
       </div>
     </div>
   );
 };
+
