0af206c0ce4ea9cb629eef9d7860dde0d0b130a8
nairut <turian@hotmail.com>
2023-07-17 19:32:37 -0300

    sdfsdf

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
-import { Desktop } from "./screens/Desktop";
+import { App } from "./App";
 
 const app = document.getElementById("app");
 const root = ReactDOMClient.createRoot(app);
-root.render(<Desktop />);
+root.render(<App />);
