835b1af22eb2c8033b9b7e5db076be029574c63d
nairut <turian@hotmail.com>
2023-07-17 17:59:32 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -15,7 +15,7 @@ export const Desktop = () => {
           </div>
 
             <button type="submit">
-            <Group className="group-2" property1="default" />
+            <Group className="group-2"  />
             </button>
 
           </form>
