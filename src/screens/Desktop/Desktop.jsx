291c8e88a4784b62897ff6f947f5887f659e6ae0
nairut <turian@hotmail.com>
2023-07-17 14:55:50 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -12,11 +12,10 @@ export const Desktop = () => {
     <div className="desktop">
       <div className="div">
         <div className="overlap-group">
-          <div className="rectangle" />
           <form name="contact" method="post" action="/contact">
-          <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="form-name" value="contact" />
             <input type="text" className="name" placeholder="Enter your name" />
-            <button type="submit">asdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçklasdfladsçkl</button>
+            <button type="submit" className="talk-to-an-expert" onClick={handleSubmit} />
           </form>
         </div>
         <Group className="group-2" property1="default" to="/thank-you" />
