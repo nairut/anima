860881ef18596fa3e20bae5b3f82abbdecb6c1a6
nairut <turian@hotmail.com>
2023-07-17 12:24:33 -0300

    Update turian.jsx

--- src/screens/Desktop/turian.jsx
+++ src/screens/Desktop/turian.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProjectForm from "../../components/ProjectForm";
 
 
-function NewProject() {
+function turian() {
     return (
       <div>
         <h1>NewProject</h1>
