a09ec3973f85100f54a4e0b60e3878b8703f4fa2
nairut <turian@hotmail.com>
2023-07-17 15:52:12 -0300

    sadfdf

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -1,30 +1,16 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Group } from "../../components/Group";
 import "./style.css";
 
 export const Desktop = () => {
-  const formRef = useRef(null);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(formRef.current);
-    const name = formData.get("name");
-    // Process the form data and perform submission logic here
-    console.log("Name:", name);
-    // Example: Make an API request to submit the form data
-  };
-
   return (
     <div className="desktop">
       <div className="div">
         <div className="overlap-group">
-          <form ref={formRef} name="contact" method="post" action="/contact">
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="rectangle" />
-            <input type="text" name="name" className="name" placeholder="Enter your name" />
-            <Group className="group-2" property1="default" to="/thank-you" onClick={handleSubmit} />
-          </form>
+          <div className="rectangle" />
+          <input className="name" />
         </div>
+        <Group className="group-2" property1="default" to="/thank-you" />
       </div>
     </div>
   );
