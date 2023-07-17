3965b84c5bd82e5451316840129a6d64abd6f860
nairut <turian@hotmail.com>
2023-07-17 15:09:09 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -12,11 +12,12 @@ export const Desktop = () => {
     <div className="desktop">
       <div className="div">
         <div className="overlap-group">
-          <div className="rectangle" />
+          
           <form name="contact" method="post" action="/contact">
           <input type="hidden" name="form-name" value="contact" />
+          <div className="rectangle" />
             <input type="text" className="name" placeholder="Enter your name" />
-            <button type="submit">sasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsdsasdsdasdasdasdasdsasdasdsd</button>
+            <button type="submit">sasdsdasdasdasdasdsasdasdsd</button>
           </form>
         </div>
         <Group className="group-2" property1="default" to="/thank-you" />
