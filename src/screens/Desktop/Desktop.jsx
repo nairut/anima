eae88b5fbe06fb24331f93c28dfd66b372ad94ac
nairut <turian@hotmail.com>
2023-07-17 16:18:13 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -14,12 +14,10 @@ export const Desktop = () => {
           <input type="hidden" name="form-name" value="contact" />
             <input type="text" className="name" placeholder="Enter your name" />
             <button type="submit" className="group-2">
-  aadsçklasdfladsçklasdfladsçklasdfladsçkl
-
-
 <div className="Group2 w-[335px] h-[55px] relative">
   <div className="Rectangle1 w-[335px] h-[55px] left-0 top-0 absolute bg-blue-800" />
-  <div className="TalkToAnExpert w-[209px] h-[9px] left-[63px] top-[23px] absolute text-center text-white text-xl font-light uppercase leading-[13px] tracking-wide">Talk to an Expert</div>
+  <div className="TalkToAnExpert w-[209px] h-[9px] left-[63px] top-[23px] 
+  absolute text-center text-white text-xl font-light uppercase leading-[13px] tracking-wide">Talk to an Expert</div>
 </div>
 
 </button>
