e23bc3f12715ece541ca83cb3fe99d232f80e5b5
nairut <turian@hotmail.com>
2023-07-17 12:10:46 -0300

    zxczxc

---
+++ src/screens/Desktop/NewProject.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectForm from "../../components/ProjectForm";
+
+
+function NewProject() {
+    return (
+      <div>
+        <h1>NewProject</h1>
+      </div>
+    )
+  }
+  
+  export default NewProject
