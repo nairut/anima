36da6d7fc36641d80bf80a9a8a81db3916eb33f6
nairut <turian@hotmail.com>
2023-07-17 11:07:26 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -3,11 +3,25 @@ import { Group } from "../../components/Group";
 import "./style.css";
 import ProjectForm from "../../components/Group";
 
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <input className="name" />
+        </div>
+        <Group className="group-2" property1="default" to="/thank-you" />
+        <ProjectForm/>
+      </div>
+    </div>
+  );
+};
 
 
 function NewProject() {
   return (
-    <div className="style.css">
+    <div className="desktop">
       <h1>só em sonho</h1>
       <ProjectForm />
     </div>
