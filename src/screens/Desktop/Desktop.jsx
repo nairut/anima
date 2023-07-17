e466ebd53b10d9b3c97662b432eb50abe9eab20a
nairut <turian@hotmail.com>
2023-07-17 16:09:04 -0300

    Update Desktop.jsx

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -13,7 +13,7 @@ export const Desktop = () => {
           <form name="contact" method="post" action="/contact">
           <input type="hidden" name="form-name" value="contact" />
             <input type="text" className="name" placeholder="Enter your name" />
-            <button type="submit" class="group default">
+            <button type="submit" class="group-2">
   aadsçklasdfladsçklasdfladsçklasdfladsçkl
 </button>
           </form>
