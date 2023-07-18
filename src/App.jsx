039b7ad987e3fb69817f33ce512599b830083ccc
nairut <turian@hotmail.com>
2023-07-18 19:33:49 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { TranslationService } from "./screens/TranslationService";
+import { Celular } from "./screens/Celular";
+import { TranslationServiceScreen } from "./screens/TranslationServiceScreen";
+
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <TranslationService />,
+  },
+  {
+    path: "/translation-service-2",
+    element: <TranslationService />,
+  },
+  {
+    path: "/celular",
+    element: <Celular />,
+  },
+  {
+    path: "/translation-service",
+    element: <TranslationServiceScreen />,
+  },
+]);
+
+export const App = () => {
+  return <RouterProvider router={router} />;
+};
---
+++ src/screens/TranslationServiceScreen/TranslationServiceScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export const TranslationServiceScreen = () => {
+  return (
+    <div className="translation-service-screen">
+      <div className="translation-service-2">
+        <div className="rectangle-5" />
+        <div className="rectangle-6" />
+        <div className="rectangle-7" />
+        <div className="rectangle-8" />
+      </div>
+    </div>
+  );
+};
