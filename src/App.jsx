76e01bbe2b98fa3c74bae76c3c7f38cdafae49f1
nairut <turian@hotmail.com>
2023-07-18 12:12:59 -0300

    adsad

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Desktop } from "./screens/Desktop";
 import { ThankYou } from "./screens/ThankYou";
 import { Index } from "./screens/Index";
 
 const router = createBrowserRouter([
   {
     path: "/*",
-    element: <Desktop />,
-  },
-  {
-    path: "/desktop-1",
-    element: <Desktop />,
+    element: <ThankYou />,
   },
   {
     path: "/thank-you",
