ae9f82c41cdd7b3193a4fbb4cb640161d701ef3a
nairut <turian@hotmail.com>
2023-07-19 15:12:13 -0300

    xccxvcvxc

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { DesktopForm } from "./screens/DesktopForm";
 import { Celular } from "./screens/Celular";
+import { DesktopForm } from "./screens/DesktopForm";
 import { CellForm } from "./screens/CellForm";
+import { Tablet } from "./screens/Tablet";
+import { Index } from "./screens/Index";
 
-const router = createBrowserRouter([
-  {
-    path: "/*",
-    element: <DesktopForm />,
-  },
-  {
-    path: "/desktop-form",
-    element: <DesktopForm />,
-  },
-  {
-    path: "/celular-all-breakpoints",
-    element: <Celular />,
-  },
-  {
-    path: "/cell-form",
-    element: <CellForm />,
-  },
-]);
 
-export const App = () => {
-  return <RouterProvider router={router} />;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: false,
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize() {
+    const isMobile = window.innerWidth <= 768; // Adjust the breakpoint as needed
+    this.setState({ isMobile });
+  }
+
+  render() {
+    const { isMobile } = this.state;
+
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {isMobile ? <Redirect to="/Celular" /> : <Redirect to="/Index" />}
+          </Route>
+          <Route exact path="/index" component={Index} />
+          <Route exact path="/desktop-form" component={DesktopForm} />
+          <Route exact path="/Celular" component={Celular} />
+          <Route exact path="/cellform" component={CellForm} />
+
+            {isMobile ? <Redirect to="/cellform" /> : <Redirect to="/desktop-form" />}
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
+
---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import { Group } from "../../components/Group";
+import { Lady } from "../../components/Lady";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div-5">
+        <Lady className="lady-2" />
+        <p className="text-wrapper-36">Trusted by Elite Businesses Worldwide</p>
+        <p className="text-wrapper-37">Costumized to Fit your Specific Industry Needs</p>
+        <img className="zapier-png" alt="Zapier png" src="/img/zapier-png-0.png" />
+        <img className="abbott-png" alt="Abbott png" src="/img/abbott-png-0.png" />
+        <img className="zf-png" alt="Zf png" src="/img/zf-png-0.png" />
+        <img className="anton-paar-png" alt="Anton paar png" src="/img/anton-paar-png-0.png" />
+        <img className="crowdin-png" alt="Crowdin png" src="/img/crowdin-png-0.png" />
+        <img className="rectangle-6" alt="Rectangle" src="/img/rectangle-13.png" />
+        <img className="cloudwords-png" alt="Cloudwords png" src="/img/cloudwords-png-0.png" />
+        <img className="rectangle-7" alt="Rectangle" src="/img/rectangle-14.png" />
+        <img className="rectangle-8" alt="Rectangle" src="/img/rectangle-7.png" />
+        <img className="rectangle-9" alt="Rectangle" src="/img/rectangle-15.png" />
+        <img className="rectangle-10" alt="Rectangle" src="/img/rectangle-8.png" />
+        <img className="evoltz-png" alt="Evoltz png" src="/img/evoltz-png-0.png" />
+        <img className="rectangle-11" alt="Rectangle" src="/img/rectangle-9.png" />
+        <img className="jungheinrich-png" alt="Jungheinrich png" src="/img/jungheinrich-png-0.png" />
+        <img className="rectangle-12" alt="Rectangle" src="/img/rectangle-10.png" />
+        <img className="shopee-png" alt="Shopee png" src="/img/shopee-png-0.png" />
+        <div className="overlap-24">
+          <div className="text-wrapper-38">Legal</div>
+          <img className="law-2" alt="Law" src="/img/law-1.png" />
+        </div>
+        <div className="overlap-25">
+          <div className="media-films-2">Media &amp; Films</div>
+          <img className="next" alt="Next" src="/img/next-1.png" />
+        </div>
+        <div className="overlap-26">
+          <div className="text-wrapper-39">Medical</div>
+          <img className="img-4" alt="Medical bag" src="/img/medical-bag-1.png" />
+        </div>
+        <div className="overlap-27">
+          <div className="text-wrapper-40">E-Learning</div>
+          <img className="img-4" alt="Mba" src="/img/mba-1.png" />
+        </div>
+        <div className="overlap-28">
+          <div className="text-wrapper-41">Manufacturing</div>
+          <img className="img-4" alt="Manufacturing" src="/img/manufacturing-1.png" />
+        </div>
+        <div className="overlap-group-5">
+          <div className="text-wrapper-42">Automotive</div>
+          <img className="img-4" alt="Tesla model x" src="/img/tesla-model-x-1.png" />
+        </div>
+        <div className="overlap-29">
+          <div className="text-wrapper-43">Financial</div>
+          <img className="money-bag-pounds-2" alt="Money bag pounds" src="/img/money-bag-pounds-1.png" />
+        </div>
+        <div className="overlap-30">
+          <div className="text-wrapper-44">Marketing</div>
+          <img className="img-4" alt="Commercial" src="/img/commercial-1.png" />
+        </div>
+        <div className="overlap-31">
+          <div className="text-wrapper-41">IT &amp; Software</div>
+          <img className="img-4" alt="Programming flag" src="/img/programming-flag-1.png" />
+        </div>
+        <div className="overlap-32">
+          <div className="text-wrapper-45">Life Science</div>
+          <img className="img-4" alt="Biotech" src="/img/biotech-2.png" />
+        </div>
+        <div className="overlap-33">
+          <div className="text-wrapper-43">Tourism</div>
+          <img className="img-4" alt="Plane" src="/img/plane-1.png" />
+        </div>
+        <div className="overlap-34">
+          <div className="text-wrapper-46">E-commerce</div>
+          <img className="img-4" alt="Buy" src="/img/buy-1.png" />
+        </div>
+        <div className="text-wrapper-47">What can you translate?</div>
+        <p className="text-wrapper-48">
+          Our proficient translators have the capability to translate any kind of document, supporting the
+          undermentioned file types
+        </p>
+        <div className="overlap-35">
+          <div className="document-translation-2">
+            Document
+            <br />
+            Translation
+          </div>
+          <img className="img-5" alt="Pdf" src="/img/pdf-1.png" />
+        </div>
+        <div className="overlap-36">
+          <div className="marketing-material-2">Marketing Material &amp; Ads</div>
+          <img className="img-5" alt="Goal" src="/img/goal-1.png" />
+        </div>
+        <div className="overlap-37">
+          <div className="website-apps-2">Website &amp; Apps</div>
+          <img className="img-5" alt="Website" src="/img/website-1.png" />
+        </div>
+        <div className="overlap-38">
+          <div className="text-wrapper-49">Video Subtitling</div>
+          <img className="img-5" alt="Video camera" src="/img/video-camera-1.png" />
+        </div>
+        <div className="overlap-39">
+          <div className="text-wrapper-50">Product Descriptions</div>
+          <img className="img-5" alt="E commerce" src="/img/e-commerce-1.png" />
+        </div>
+        <div className="overlap-40">
+          <div className="text-wrapper-51">Articles, certificates, and more</div>
+          <img className="biotech-2" alt="Biotech" src="/img/biotech-2.png" />
+        </div>
+        <div className="group-10">
+          <h1 className="text-wrapper-52">Professional Translation Services</h1>
+          <p className="element-languages-that-3">
+            120 languages that fits in your budget
+            <br />
+            Human Translation done with the highest quality in the market
+            <br />
+            Linguists with Master&#39;s Degrees
+          </p>
+          <Group className="group-11" property1="default" talkToAnExpertClassName="group-12" to="/desktop-form" />
+        </div>
+        <Group className="group-13" property1="default" talkToAnExpertClassName="group-12" to="/desktop-form" />
+        <Group className="group-14" property1="default" talkToAnExpertClassName="group-12" to="/desktop-form" />
+        <p className="our-growth-would-not-2">
+          Our Growth would not have been possible without Magma Translation. <br />
+          Our internationalization occurred through the expertise of their team.
+        </p>
+        <p className="text-wrapper-53">
+          What we were looking for in Magma was the cultural localization, their deep understanding of how people think
+          in other countries, specially of the US, UK and Germany for our projects
+        </p>
+        <p className="we-are-a-german-2">
+          <span className="text-wrapper-54">
+            We are a German company, the largest auto parts manufacturer in the world. For us, trust in data security is{" "}
+          </span>
+          <span className="text-wrapper-55">essential</span>
+          <span className="text-wrapper-54">. Magma Translation is a trusted company.</span>
+        </p>
+        <div className="text-wrapper-56">Highest Quality Guaranteed</div>
+        <p className="every-translation-2">
+          Every translation undergoes thorough proofreading and validation by <br />
+          both our team of professional external editors and our internal language specialists.
+        </p>
+        <div className="text-wrapper-57">Shopee.com</div>
+        <div className="text-wrapper-58">Hotmart</div>
+        <div className="text-wrapper-59">ZF Friedrichshafen</div>
+        <div className="text-wrapper-60">Milton Santana</div>
+        <div className="text-wrapper-61">Bruno Vidal</div>
+        <div className="text-wrapper-62">Danielle Schmitt Capote</div>
+        <img className="ellipse-4" alt="Ellipse" src="/img/ellipse-1-1.png" />
+        <img className="ellipse-5" alt="Ellipse" src="/img/ellipse-2-1.png" />
+        <img className="ellipse-6" alt="Ellipse" src="/img/ellipse-3-1.png" />
+        <img className="star-16" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-17" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-18" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-19" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-20" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-21" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-22" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-23" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-24" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-25" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-26" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-27" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-28" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-29" alt="Star" src="/img/star-16-1.png" />
+        <img className="star-30" alt="Star" src="/img/star-16-1.png" />
+        <img className="magma-translation-6" alt="Magma translation" src="/img/magma-translation-png-1.png" />
+        <p className="text-wrapper-63">
+          MAGMA is a global localization solutions provider, enabling brands to establish an authentic presence in
+          diverse markets around the world.
+        </p>
+        <p className="text-wrapper-64">Magma Translation | All rights reserved. ©Magma™</p>
+        <img className="magma-translation-7" alt="Magma translation" src="/img/magma-translation-png-1.png" />
+      </div>
+    </div>
+  );
+};
