858b7e309b2226493099f8a569a0b36fe02e7c53
nairut <turian@hotmail.com>
2023-07-19 12:48:31 -0300

    asdfasdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { DesktopForm } from "./screens/DesktopForm";
-import { Celular } from "./screens/Celular";
-import { CellForm } from "./screens/CellForm";
+import { Index } from "./screens/Index";
 
 const router = createBrowserRouter([
   {
     path: "/*",
-    element: <Celular />,
+    element: <DesktopForm />,
   },
   {
     path: "/desktop-form",
     element: <DesktopForm />,
   },
-
   {
-    path: "/cell-form",
-    element: <CellForm />,
+    path: "/index",
+    element: <Index />,
   },
 ]);
 
