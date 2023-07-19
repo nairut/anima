cdab682acc104674d61517d12856b43f4514c3d1
nairut <turian@hotmail.com>
2023-07-19 15:27:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Celular } from "./screens/Celular";
-import { DesktopForm } from "./screens/DesktopForm";
-import { CellForm } from "./screens/CellForm";
-import { Tablet } from "./screens/Tablet";
-import { Index } from "./screens/Index";
+import React from 'react';
+import ResponsiveComponent from './ResponsiveComponent';
 
-const router = createBrowserRouter([
-  {
-    path: "/*",
-    element: <Celular />,
-  },
-  {
-    path: "/celular",
-    element: <Celular />,
-  },
-  {
-    path: "/desktop-form",
-    element: <DesktopForm />,
-  },
-  {
-    path: "/cell-form",
-    element: <CellForm />,
-  },
-  {
-    path: "/tablet",
-    element: <Tablet />,
-  },
-  {
-    path: "/index",
-    element: <Index />,
-  },
-]);
-
-export const App = () => {
-  return <RouterProvider router={router} />;
+const App = () => {
+  return (
+    <div>
+      <ResponsiveComponent />
+    </div>
+  );
 };
+
+export default App;
