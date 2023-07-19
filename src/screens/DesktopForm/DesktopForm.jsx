8c200d1966f7ac1ebade928532a8fc234c7cec48
nairut <turian@hotmail.com>
2023-07-19 12:23:21 -0300

    Update DesktopForm.jsx

--- src/screens/DesktopForm/DesktopForm.jsx
+++ src/screens/DesktopForm/DesktopForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Lady } from "./Lady";
+import { Lady } from "../../components/Lady";
 import "./style.css";
 
 export const DesktopForm = () => {
@@ -8,7 +8,7 @@ export const DesktopForm = () => {
       <div className="div">
         <div className="overlap">
           <div className="rectangle" />
-          <div className="group">
+          <div className="overlap-wrapper">
             <div className="overlap-group">
               <div className="text-wrapper">First name</div>
             </div>
@@ -16,15 +16,15 @@ export const DesktopForm = () => {
         </div>
         <div className="overlap-2">
           <div className="rectangle-2" />
-          <div className="overlap-wrapper">
+          <div className="overlap-group-wrapper">
             <div className="div-wrapper">
               <div className="text-wrapper-2">Email</div>
             </div>
           </div>
         </div>
         <div className="overlap-3">
-          <img className="img" alt="Rectangle" src="rectangle-6.svg" />
-          <div className="overlap-group-wrapper">
+          <img className="img" alt="Rectangle" src="/img/rectangle-6-1.png" />
+          <div className="group-2">
             <div className="overlap-4">
               <div className="rectangle-3" />
               <div className="text-wrapper-3">Message</div>
@@ -32,23 +32,22 @@ export const DesktopForm = () => {
           </div>
         </div>
         <div className="overlap-5">
-          <div className="rectangle-4" />
-          <div className="group-2">
+          <div className="rectangle" />
+          <div className="group-3">
             <div className="overlap-group">
               <div className="text-wrapper-4">Last name</div>
             </div>
           </div>
         </div>
-        <div className="group-3">
+        <div className="group-4">
           <div className="overlap-group-2">
             <div className="text-wrapper-5">Send</div>
           </div>
         </div>
-        <img className="line" alt="Line" src="line-1.svg" />
+        <img className="line" alt="Line" src="/img/line-1-1.png" />
         <Lady className="lady-instance" />
-        <img className="magma-translation" alt="Magma translation" src="magma-translation-png.png" />
+        <img className="magma-translation" alt="Magma translation" src="/img/magma-translation-png-13.png" />
         <h1 className="h-1">Get a Free, No-Obligation Price Quote</h1>
-        <p className="p">Get a Free, No-Obligation Price Quote</p>
       </div>
     </div>
   );
