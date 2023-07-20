ce0b35208ffed3d935c92cd05e2b2fa463b2973a
nairut <turian@hotmail.com>
2023-07-20 12:23:25 -0300

    Update DesktopForm.jsx

--- src/screens/DesktopForm/DesktopForm.jsx
+++ src/screens/DesktopForm/DesktopForm.jsx
@@ -37,10 +37,10 @@ export const DesktopForm = () => {
 
         <div className="overlap-3">
           <img className="img" alt="Rectangle" src="/img/rectangle-6.png" />
-          <textarea required name="message" placeholder="Message" className="rectangle-3">   </textarea>
           <div className="group-2">
             <div className="overlap-4">
-              <div className="rectangle-3" />
+              <textarea required name="message" placeholder="Message" className="rectangle-3">   </textarea>
+
               <div className="text-wrapper-3">Message</div>
             </div>
           </div>
