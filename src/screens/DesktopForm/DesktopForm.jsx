158ef804ea45d550d97618ca0e22e8de184fd40a
nairut <turian@hotmail.com>
2023-07-20 12:34:31 -0300

    asdsd

--- src/screens/DesktopForm/DesktopForm.jsx
+++ src/screens/DesktopForm/DesktopForm.jsx
@@ -6,33 +6,42 @@ export const DesktopForm = () => {
   return (
     <div className="desktop-form">
       <div className="div">
-        <div className="overlap">
-          <div className="rectangle" />
-          <div className="group">
-            <div className="overlap-group">
-              <div className="text-wrapper">First name</div>
-            </div>
+        <div className="group">
+          <div className="overlap-group">
+            <div className="text-wrapper">Send</div>
           </div>
         </div>
-        <div className="overlap-2">
-          <div className="rectangle-2" />
+        <div className="overlap">
+          <div className="rectangle" />
           <div className="overlap-wrapper">
-            <div className="div-wrapper">
-              <div className="text-wrapper-2">Email</div>
+            <div className="overlap-2">
+              <div className="rectangle-2" />
+              <div className="text-wrapper-2">Message</div>
             </div>
           </div>
         </div>
         <div className="overlap-3">
           <div className="rectangle-3" />
           <div className="overlap-group-wrapper">
-            <div className="overlap-group">
-              <div className="text-wrapper-3">Last name</div>
+            <div className="div-wrapper">
+              <div className="text-wrapper-3">Email</div>
             </div>
           </div>
         </div>
-        <div className="group-2">
-          <div className="overlap-group-2">
-            <div className="text-wrapper-4">Send</div>
+        <div className="overlap-4">
+          <div className="rectangle-4" />
+          <div className="group-2">
+            <div className="overlap-5">
+              <div className="text-wrapper-4">Last name</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-6">
+          <div className="rectangle-5" />
+          <div className="group-3">
+            <div className="overlap-5">
+              <div className="text-wrapper-5">First name</div>
+            </div>
           </div>
         </div>
         <img className="line" alt="Line" src="/img/line-1.png" />
@@ -43,17 +52,8 @@ export const DesktopForm = () => {
           MAGMA is a global localization solutions provider, enabling brands to establish an authentic presence in
           diverse markets around the world.
         </p>
-        <p className="text-wrapper-5">Magma Translation | All rights reserved. ©Magma™</p>
+        <p className="text-wrapper-6">Magma Translation | All rights reserved. ©Magma™</p>
         <img className="img" alt="Magma translation" src="/img/magma-translation-png-1.png" />
-        <div className="overlap-4">
-          <div className="rectangle-4" />
-          <div className="group-3">
-            <div className="overlap-5">
-              <div className="rectangle-5" />
-              <div className="text-wrapper-6">Message</div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
